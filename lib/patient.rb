e3d6d46964e436847ef07bf8c472a1015b6efc52
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-19 17:45:51 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,23 +1,23 @@
 # require 'pry'
 # class Patient
 #   attr_accessor :patient
-# 
+#
 #   @@all = []
-# 
+#
 #   def initialize(patient)
 #     @patient = patient
 #     @@all << self
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def new_appointment(doctor, date)
 #     Appointment.new(doctor, self, date)
 #     binding.pry
 #   end
-# 
+#
 # end
 
 class Patient

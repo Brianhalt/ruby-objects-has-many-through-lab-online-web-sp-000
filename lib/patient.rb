f7c9f615226b8a2716024bf5a27c00526f901d4a
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-19 18:37:25 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,32 +17,20 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
-  # def appointments
-  #   Appointment.all.select do |doctor|
-  #     appointment.doctor == self
-  #   end
-  # end
-  #
-  # def patients
-  #   Appointment.all.select do |doctor|
-  #     appointment.doctor == self
-  #   end
-  # end
-
-  # def appointments
-  #   theappointments = []
-  #   self.appointments.each do |doctor|
-  #     thedoctors << appointment.doctor
-  #   end
-  #   return thedoctors
-  # end
-  #
-  # def patients
-  #   thepatients = []
-  #   self.appointments.each do |appointment|
-  #     thepatients << appointment.patient
-  #   end
-  #   return thepatients
-  # end
+  def appointments
+    theappointments = []
+    self.appointments.each do |date|
+      theappointments << appointment.date
+    end
+    return theappointments
+  end
+  
+  def doctors
+    thedoctors = []
+    self.appointments.each do |doctor|
+      thepatients << appointment.patient
+    end
+    return thepatients
+  end
 
 end
